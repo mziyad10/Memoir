b36dc2bbc0c6b0a5882a9a54edeef193f0f0539a
mziyad10 <mvsiyad10@gmail.com>
2025-07-13 12:39:25 +0530

    completed breadcrumb

--- src/Pages/Homepage.jsx
+++ src/Pages/Homepage.jsx
@@ -1,9 +1,18 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const HomePage = () => {
   return (
-    <div>
-      
+    <div className='mt-4 flex flex-col gap-4'>
+      {/* BREADCRUMB */}
+      <div className="flex gap-3">
+        <Link to="/">Home</Link>
+        <span>•</span>
+        <span className='text-blue-800'>Blogs and Articles</span>
+      </div>
+      {/* INTRODUCTION */}
+      {/* FEATURED POSTS */}
+      {/* POST LIST */}
     </div>
   )
 }
