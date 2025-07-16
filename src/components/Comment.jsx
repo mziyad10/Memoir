05e6fc9bd7e8e08e26ee83582ff1f44fe28427b9
mziyad10 <mvsiyad10@gmail.com>
2025-07-16 17:02:00 +0530

    Added comments to post page

--- src/components/Comment.jsx
+++ src/components/Comment.jsx
@@ -2,8 +2,15 @@ import React from 'react'
 
 const Comment = () => {
   return (
-    <div>
-      
+    <div className='p-4 bg-slate-50 rounded-xl mb-8'>
+    <div className="flex items-center gap-4">
+        <img src="userImg.jpeg" className="w-10 h-10 rounded-full object-cover" w="40" />
+        <span className='font-medium'>John Doe</span>
+        <span className='text-sm text-gray-500'>2 days ago</span>
+    </div>
+      <div className="mt-4">
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim explicabo eos minima repellat ex hic autem nulla. Dolore veritatis odit porro corrupti quam iste doloribus.</p>
+      </div>
     </div>
   )
 }
